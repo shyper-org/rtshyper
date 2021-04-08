13976fcec77a3cddca561f57079eb684d93962dc
Siran Li <953760237@qq.com>
2021-04-09 00:15:44 +0800

    feat: change array to vec

--- src/config/qemu_def.rs
+++ src/config/qemu_def.rs
@@ -1,8 +1,11 @@
 const NAME_MAX_LEN: usize = 32;
 const PASSTHROUGH_DEV_MAX_NUM: usize = 128;
 const EMULATED_DEV_MAX_NUM: usize = 16;
+
 use crate::kernel::VM_MEM_REGION_MAX;
 use crate::kernel::VM_NUM_MAX;
+use alloc::vec::Vec;
+use spin::Mutex;
 
 pub enum VmType {
     VmTOs = 0,
@@ -28,7 +31,7 @@ impl VmRegion {
 
 pub struct VmMemoryConfig {
     num: u32,
-    region: Option<[VmRegion; VM_MEM_REGION_MAX]>,
+    region: Option<Vec<VmRegion>>,
 }
 
 impl VmMemoryConfig {
@@ -41,12 +44,12 @@ impl VmMemoryConfig {
 }
 
 pub struct VmImageConfig {
-    kernel_name: Option<[u8; NAME_MAX_LEN]>,
+    kernel_name: Option<&'static str>,
     kernel_load_ipa: usize,
     kernel_entry_point: usize,
-    device_tree_filename: Option<[u8; NAME_MAX_LEN]>,
+    device_tree_filename: Option<&'static str>,
     device_tree_load_ipa: usize,
-    ramdisk_filename: Option<[u8; NAME_MAX_LEN]>,
+    ramdisk_filename: Option<&'static str>,
     ramdisk_load_ipa: usize,
 }
 
@@ -81,15 +84,13 @@ impl VmCpuConfig {
 }
 
 pub struct VmConfigEntry {
-    pub name: Option<[u8; NAME_MAX_LEN]>,
+    pub name: Option<&'static str>,
     pub os_type: VmType,
     pub memory: VmMemoryConfig,
     pub image: VmImageConfig,
     pub cpu: VmCpuConfig,
-    pub emu_dev_num: usize,
-    pub vm_emu_dev_confg: Option<[VmEmulatedDeviceConfig; EMULATED_DEV_MAX_NUM]>,
-    pub pt_dev_num: usize,
-    pub vm_pt_dev_confg: Option<[VmPassthroughDeviceConfig; PASSTHROUGH_DEV_MAX_NUM]>,
+    pub vm_emu_dev_confg: Option<Vec<VmEmulatedDeviceConfig>>,
+    pub vm_pt_dev_confg: Option<Vec<VmPassthroughDeviceConfig>>,
 }
 
 impl VmConfigEntry {
@@ -100,64 +101,68 @@ impl VmConfigEntry {
             memory: VmMemoryConfig::default(),
             image: VmImageConfig::default(),
             cpu: VmCpuConfig::default(),
-            emu_dev_num: 0,
             vm_emu_dev_confg: None,
-            pt_dev_num: 0,
             vm_pt_dev_confg: None,
         }
     }
 }
 
 pub struct VmConfigTable {
-    pub name: Option<[u8; NAME_MAX_LEN]>,
+    pub name: Option<&'static str>,
     pub vm_num: usize,
-    pub entries: Option<[VmConfigEntry; VM_NUM_MAX]>,
+    pub entries: Vec<VmConfigEntry>,
 }
 
-static DEF_VM_CONFIG_TABLE: VmConfigTable = VmConfigTable {
-    name: None,
-    vm_num: 1,
-    entries: Some([
-        VmConfigEntry {
+impl VmConfigTable {
+    const fn default() -> VmConfigTable {
+        VmConfigTable {
             name: None,
-            os_type: VmType::VmTOs,
-            memory: VmMemoryConfig {
-                num: 1,
-                region: Some([
-                    VmRegion {
-                        ipa_start: 0x50000000,
-                        length: 0x80000000,
-                    },
-                    VmRegion::default(),
-                    VmRegion::default(),
-                    VmRegion::default(),
-                ]),
-            },
-            image: VmImageConfig {
-                kernel_name: None,
-                kernel_load_ipa: 0x58080000,
-                kernel_entry_point: 0x58080000,
-                device_tree_filename: None,
-                device_tree_load_ipa: 0x52000000,
-                ramdisk_filename: None,
-                ramdisk_load_ipa: 0x53000000,
-            },
-            cpu: VmCpuConfig {
-                num: 4,
-                allocate_bitmap: 0b1111,
-                master: 0,
-            },
-            emu_dev_num: 0,
-            vm_emu_dev_confg: None,
-            pt_dev_num: 0,
-            vm_pt_dev_confg: None,
+            vm_num: 0,
+            entries: Vec::new(),
+        }
+    }
+}
+
+pub static DEF_VM_CONFIG_TABLE: Mutex<VmConfigTable> = Mutex::new(VmConfigTable::default());
+
+pub fn config_init() {
+    let mut vm_config = DEF_VM_CONFIG_TABLE.lock();
+    vm_config.name = Some("qemu-default");
+    vm_config.vm_num = 1;
+
+    // vm0
+    let mut emu_dev_config: Vec<VmEmulatedDeviceConfig> = Vec::new();
+    emu_dev_config.push(VmEmulatedDeviceConfig {});
+    let mut pt_dev_config: Vec<VmPassthroughDeviceConfig> = Vec::new();
+    pt_dev_config.push(VmPassthroughDeviceConfig {});
+    let mut vm_region: Vec<VmRegion> = Vec::new();
+    vm_region.push(VmRegion {
+        ipa_start: 0x50000000,
+        length: 0x80000000,
+    });
+
+    vm_config.entries.push(VmConfigEntry {
+        name: Some("supervisor"),
+        os_type: VmType::VmTOs,
+        memory: VmMemoryConfig {
+            num: 1,
+            region: Some(vm_region),
         },
-        VmConfigEntry::default(),
-        VmConfigEntry::default(),
-        VmConfigEntry::default(),
-        VmConfigEntry::default(),
-        VmConfigEntry::default(),
-        VmConfigEntry::default(),
-        VmConfigEntry::default(),
-    ]),
-};
+        image: VmImageConfig {
+            kernel_name: Some("Image"),
+            kernel_load_ipa: 0x58080000,
+            kernel_entry_point: 0x58080000,
+            device_tree_filename: Some("qemu1.bin"),
+            device_tree_load_ipa: 0x52000000,
+            ramdisk_filename: Some("initrd.gz"),
+            ramdisk_load_ipa: 0x53000000,
+        },
+        cpu: VmCpuConfig {
+            num: 4,
+            allocate_bitmap: 0b1111,
+            master: 0,
+        },
+        vm_emu_dev_confg: Some(emu_dev_config),
+        vm_pt_dev_confg: Some(pt_dev_config),
+    });
+}
--- src/vmm/config.rs
+++ src/vmm/config.rs
@@ -1,3 +1,13 @@
+use crate::config::DEF_VM_CONFIG_TABLE;
+
 pub fn vmm_init_config() {
+    crate::config::config_init();
+    // let config_table = DEF_VM_CONFIG_TABLE.lock();
+    // println!("entries num {}", config_table.entries.len());
+    // if let Some(x) = config_table.entries[0].name {
+    //     println!("{}", x);
+    // }
+    // println!("vm num {}", config_table.vm_num);
+
     println!("VM config init ok");
 }
