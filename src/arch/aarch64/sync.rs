da4419034cfb8dec7c0b979f4a3ef046fbc74cc0
Siran Li <953760237@qq.com>
2022-04-12 16:43:43 +0800

    fix: fix code style in hvc_handler & count hvc running time

--- src/arch/aarch64/sync.rs
+++ src/arch/aarch64/sync.rs
@@ -11,6 +11,9 @@ use crate::arch::exception_next_instruction_step;
 use crate::arch::smc_guest_handler;
 use crate::device::{emu_handler, EmuContext};
 use crate::kernel::{current_cpu, hvc_guest_handler, migrate_data_abort_handler};
+use crate::lib::time_current_us;
+
+pub const HVC_RETURN_REG: usize = 0;
 
 pub fn data_abort_handler() {
     let emu_ctx = EmuContext {
@@ -87,38 +90,28 @@ pub fn smc_handler() {
 }
 
 pub fn hvc_handler() {
-    let idx = 0;
-    let x0 = current_cpu().get_gpr(idx);
-    let idx = 1;
-    let x1 = current_cpu().get_gpr(idx);
-    let idx = 2;
-    let x2 = current_cpu().get_gpr(idx);
-    let idx = 3;
-    let x3 = current_cpu().get_gpr(idx);
-    let idx = 4;
-    let x4 = current_cpu().get_gpr(idx);
-    let idx = 5;
-    let x5 = current_cpu().get_gpr(idx);
-    let idx = 6;
-    let x6 = current_cpu().get_gpr(idx);
-    let idx = 7;
-    let mode = current_cpu().get_gpr(idx);
+    let time_start = time_current_us();
+    let x0 = current_cpu().get_gpr(0);
+    let x1 = current_cpu().get_gpr(1);
+    let x2 = current_cpu().get_gpr(2);
+    let x3 = current_cpu().get_gpr(3);
+    let x4 = current_cpu().get_gpr(4);
+    let x5 = current_cpu().get_gpr(5);
+    let x6 = current_cpu().get_gpr(6);
+    let mode = current_cpu().get_gpr(7);
 
     let hvc_type = (mode >> 8) & 0xff;
     let event = mode & 0xff;
 
-    let idx = 0;
-    let val = 0;
-    current_cpu().set_gpr(idx, val);
     match hvc_guest_handler(hvc_type, event, x0, x1, x2, x3, x4, x5, x6) {
         Ok(val) => {
-            current_cpu().set_gpr(idx, val);
+            current_cpu().set_gpr(HVC_RETURN_REG, val);
         }
         Err(_) => {
             println!("Failed to handle hvc request fid 0x{:x} event 0x{:x}", hvc_type, event);
-            let idx = 0;
-            let val = usize::MAX;
-            current_cpu().set_gpr(idx, val);
+            current_cpu().set_gpr(HVC_RETURN_REG, 0);
         }
     }
+    let time_end = time_current_us();
+    println!("hvc fid 0x{:x} event 0x{:x} cost time {}us", hvc_type, event, time_end - time_start);
 }
