03fa2f62b1e1b7cf646817124cf527f575ab6bcf
migu4917 <491745115@qq.com>
2022-07-18 23:44:57 +0800

    fix: wrong static zero array declaration leads to larger product size

--- src/mm/heap.rs
+++ src/mm/heap.rs
@@ -3,12 +3,12 @@ use buddy_system_allocator::LockedHeap;
 // rCore buddy system allocator
 use crate::arch::PAGE_SIZE;
 
-const HEAP_SIZE: usize = 1024 * PAGE_SIZE;
+const HEAP_SIZE: usize = 10 * 1024 * PAGE_SIZE; // 40MB
 
 #[repr(align(4096))]
 struct HeapRegion([u8; HEAP_SIZE]);
 
-static HEAP_REGION: HeapRegion = HeapRegion([0; HEAP_SIZE]);
+static mut HEAP_REGION: HeapRegion = HeapRegion([0; HEAP_SIZE]);
 
 #[global_allocator]
 pub static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::empty();
@@ -24,5 +24,5 @@ pub fn heap_init() {
 
 #[alloc_error_handler]
 fn alloc_error_handler(_: core::alloc::Layout) -> ! {
-    panic!("alloc_error_handler: heap panic");
+    panic!("alloc_error_handler: heap Out Of Memory");
 }
