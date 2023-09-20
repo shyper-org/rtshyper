15de36fed3a6243644ae8027c2ee851867645d33
moce0627 <491745115@qq.com>
2023-09-20 10:43:34 +0800

    feat: remove buddy system deps, use public crate and our own locked heap

--- src/mm/heap.rs
+++ src/mm/heap.rs
@@ -1,5 +1,37 @@
-use buddy_system_allocator::LockedHeap;
+use buddy_system_allocator::Heap;
 use core::ops::RangeInclusive;
+use spin::Mutex;
+
+pub struct LockedHeap<const ORDER: usize>(Mutex<Heap<ORDER>>);
+
+impl<const ORDER: usize> LockedHeap<ORDER> {
+    /// Creates an empty heap
+    pub const fn empty() -> Self {
+        LockedHeap(Mutex::new(Heap::<ORDER>::new()))
+    }
+}
+
+impl<const ORDER: usize> core::ops::Deref for LockedHeap<ORDER> {
+    type Target = Mutex<Heap<ORDER>>;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+unsafe impl<const ORDER: usize> alloc::alloc::GlobalAlloc for LockedHeap<ORDER> {
+    unsafe fn alloc(&self, layout: alloc::alloc::Layout) -> *mut u8 {
+        self.0
+            .lock()
+            .alloc(layout)
+            .map_or(core::ptr::null_mut(), |allocation| allocation.as_ptr())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: alloc::alloc::Layout) {
+        self.0.lock().dealloc(core::ptr::NonNull::new_unchecked(ptr), layout)
+    }
+}
 
 #[global_allocator]
 pub static HEAP_ALLOCATOR: LockedHeap<{ usize::BITS as usize }> = LockedHeap::empty();
